6f82f87cb403aa1778b07064bbd2ac61be14c380
whaehofk521 <whaehofk521@gmail.com>
2024-06-20 15:46:45 +0900

    이름 변경

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "Jiyoung",
+    username: "JiYoung",
     company: "weniv",
     position: "CEO",
     img: "img/user/profile-licat.png",
